12412e37f48ec6ccb02c9220e084b613e7532230
yshunl <44795757+yshunl@users.noreply.github.com>
2019-04-15 23:18:25 +0800

    Delete delete_country.php

--- delete_country.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('dbconfig.php');
-    $country_id = $_REQUEST['country_id'];
-    $sql="DELETE FROM country WHERE country_id='$country_id'";
-    $result = $conn->query($sql);
-	echo "<meta http-equiv='refresh' content='0;url=select_country.php'>";
-?>
