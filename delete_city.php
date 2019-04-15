83b03e796302d6e4ffd5f5ee16dc4e26d377ace0
yshunl <44795757+yshunl@users.noreply.github.com>
2019-04-15 23:18:18 +0800

    Delete delete_city.php

--- delete_city.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('dbconfig.php');
-    $city = $_REQUEST['city_id'];
-    $sql="DELETE FROM city WHERE city_id='$city_id'";
-    $result = $conn->query($sql);
-	echo "<meta http-equiv='refresh' content='0;url=select_city.php'>";
-?>
