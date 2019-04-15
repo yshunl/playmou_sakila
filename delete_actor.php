6d418fd49e28349868581aaa5c77a6c01076ea28
yshunl <44795757+yshunl@users.noreply.github.com>
2019-04-15 23:17:54 +0800

    Delete delete_actor.php

--- delete_actor.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('dbconfig.php');
-    $actor_id = $_REQUEST['actor_id'];
-    $sql="DELETE FROM actor WHERE actor_id='$actor_id'";
-    $result = $conn->query($sql);
-    echo "<meta http-equiv='refresh' content='0;url=select_actor.php'>";
-?>
