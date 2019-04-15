b455846be452a76661b00958cbde49a4e7966d63
yshunl <44795757+yshunl@users.noreply.github.com>
2019-04-15 23:18:02 +0800

    Delete delete_address.php

--- delete_address.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('dbconfig.php');
-    $address_id = $_REQUEST['address_id'];
-    $sql="DELETE FROM address WHERE address_id='$address_id'";
-    $result = $conn->query($sql);
-	echo "<meta http-equiv='refresh' content='0;url=select_address.php'>";
-?>
